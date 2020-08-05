1c30af07a7916b4a7b6909d9f5f816cb5fd66746
andersonfebs <andersonfebs@gmail.com>
2020-08-05 06:35:21 -0400

    Inicio aula 3

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import PageDefault from "../../../components/PageDefault";
 
 function CadastroCategoria() {
+
+    const [categorias, setCategorias] = useState(['Teste']);
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes');    
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
+            <form onSubmit={function handleSubmit(event) {
+                event.preventDefault();
+                console.log('Tentativa de envio');
+                setCategorias([
+                    ...categorias,
+                    nomeDaCategoria3
+                ])
+            }}>
+                <label>
+                    Nome da Categoria:
+                <input
+                    type="text"
+                    value={nomeDaCategoria}
+                    onChange={function(event) {
+                        console.log('[nomeDaCatergoria]:', nomeDaCategoria);
+                        setNomeDaCategoria(event.target.value);
+                    }}
+                />
+                </label>
+
+                <button>
+                    Cadastrar
+                </button>
+            </form>
+
+            <ul>
+                {categorias.map((categoria) => {
+                    return (
+                        <li key={categoria}>
+                            {categoria}
+                        </li>
+                    )
+                })}
+            </ul>
 
             <Link to="/">
                 Ir para home
